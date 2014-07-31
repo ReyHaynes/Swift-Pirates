6fe4f0a7d064c8bbf8ac0f705d6a530034816833
Reinaldo Haynes <reinaldoh@complex.com>
2014-07-31 16:50:09 -0400

    Creating tile object.

---
+++ Swift Pirate Adventure/Tile.swift
@@ -0,0 +1,17 @@
+//
+//  Tile.swift
+//  Swift Pirate Adventure
+//
+//  Created by Reinaldo Haynes on 7/31/14.
+//  Copyright (c) 2014 Reinaldo Haynes. All rights reserved.
+//
+
+import UIKit
+
+class Tile: NSObject {
+    
+    var story:String!
+    var backgroundImage:UIImage!
+    var actionButtonName:String!
+   
+}
