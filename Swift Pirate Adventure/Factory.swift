404904bd78abcf978dbf0d2ec88f06698338f829
Reinaldo Haynes <reinaldoh@complex.com>
2014-07-31 17:21:18 -0400

    Creating factory object.

---
+++ Swift Pirate Adventure/Factory.swift
@@ -0,0 +1,13 @@
+//
+//  Factory.swift
+//  Swift Pirate Adventure
+//
+//  Created by Reinaldo Haynes on 7/31/14.
+//  Copyright (c) 2014 Reinaldo Haynes. All rights reserved.
+//
+
+import UIKit
+
+class Factory: NSObject {
+   
+}
