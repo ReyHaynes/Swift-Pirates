f72b0d410318a3266e805d930eb12b696068f2eb
Reinaldo Haynes <reinaldoh@complex.com>
2014-07-31 16:42:27 -0400

    Hooking up storyboard to view controller.

--- Swift Pirate Adventure/ViewController.swift
+++ Swift Pirate Adventure/ViewController.swift
@@ -19,7 +19,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBOutlet var backgrandImageView: UIImageView!
+    
+    @IBOutlet var healthLabel: UILabel!
+    @IBOutlet var damageLabel: UILabel!
+    @IBOutlet var weaponLabel: UILabel!
+    @IBOutlet var armorLabel: UILabel!
 
+    @IBOutlet var actionButton: UIButton!
+    
+    @IBOutlet var storyLabel: UILabel!
+    
+    @IBOutlet var northButton: UIButton!
+    @IBOutlet var southButton: UIButton!
+    @IBOutlet var westButton: UIButton!
+    @IBOutlet var eastButton: UIButton!
+    
+    
+    @IBAction func actionButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func northButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func southButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func eastButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func westButtonPressed(sender: UIButton) {
+    }
+    
 
 }
 
